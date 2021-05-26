1aa07c12d3e11b23bc88e0fb0d9366a6fc27077e
CodingSeb <sebgeiser@gmail.com>
2021-05-26 19:35:57 +0200

    Add project files.

---
+++ CSharpFilesMerger/CSharpFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpFilesMerger
+{
+    /// <summary>
+    /// -- Describe here to what is this class used for. (What is it's purpose) --
+    /// </summary>
+    public class CSharpFile
+    {
+
+    }
+}
