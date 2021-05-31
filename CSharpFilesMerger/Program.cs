dcae6b21d603c82f049ab8c44503e89e05264d05
Sébastien Geiser <sgeiser@precitrame.com>
2021-05-31 12:59:54 +0200

    args management

--- CSharpFilesMerger/Program.cs
+++ CSharpFilesMerger/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -12,30 +13,111 @@ namespace CSharpFilesMerger
         /// </summary>
         public static void Main(string[] args)
         {
+            #region Variables declaration and default values
+
             List<string> ignoreFile = new List<string>()
             {
                 @"\\Properties\\",
                 @"\\obj\\"
             };
 
-            string directory = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
+            List<string> usings = new List<string>();
+            IEnumerable<string> cSharpFileNames;
+            List<CSharpFile> cSharpFiles;
+            string mergedFileContent = string.Empty;
 
-            List<CSharpFile> cSharpFiles = Directory
-                .GetFiles(directory, "*.cs", SearchOption.AllDirectories)
-                .Where(fileName => !ignoreFile.Any(pattern => Regex.IsMatch(fileName, pattern, RegexOptions.IgnoreCase)))
+            string directory = Directory.GetCurrentDirectory();
+            bool recurcive = false;
+            string outputFileName = "Merged.cs";
+
+            #endregion
+
+            #region Decode args
+
+            #region Working directory
+
+            if(args.Length > 0 && !args[0].StartsWith("-"))
+            {
+                directory = Path.GetFullPath(Path.IsPathRooted(args[0]) ? args[0] : Path.Combine(directory, args[0]));
+            }
+
+            #endregion
+
+            #region Search files recursively ?
+
+            recurcive = args.Contains("-r");
+
+            #endregion
+
+            #region listOfFiles
+
+            int listIndex = args.ToList().IndexOf("-l");
+
+            if (listIndex > -1 && listIndex < args.Length)
+            {
+                cSharpFileNames = args[listIndex + 1].Split(';')
+                    .Select(fileName => Path.GetFullPath(Path.IsPathRooted(fileName) ? fileName : Path.Combine(directory, fileName)));
+            }
+            else
+            {
+                cSharpFileNames = Directory
+                    .GetFiles(directory, "*.cs", recurcive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
+                    .Where(fileName => !ignoreFile.Any(pattern => Regex.IsMatch(fileName, pattern, RegexOptions.IgnoreCase)));
+            }
+
+            #endregion
+
+            #region Output fileName
+
+            int outputIndex = args.ToList().IndexOf("-o");
+
+            if (outputIndex > -1 && outputIndex < args.Length)
+            {
+                outputFileName = args[outputIndex + 1];
+            }
+
+            if (!Path.IsPathRooted(outputFileName))
+            {
+                outputFileName = Path.GetFullPath(Path.Combine(directory, outputFileName));
+            }
+
+            #endregion
+
+            #endregion
+            
+            #region Get Files to Merge
+
+            Console.WriteLine($"Search files in \"{directory}\"{(recurcive ? " recursively" : string.Empty)}");
+
+            cSharpFiles = cSharpFileNames
                 .Select(fileName => new CSharpFile(fileName))
                 .ToList();
 
-            List<string> usings = new List<string>();
+            #endregion
 
-            string mergedFile = string.Empty;
+            #region Parse Files to Merge
 
             cSharpFiles.ForEach(file =>
             {
+                Console.WriteLine($"Parse file : \"{file.FileName}\"");
                 usings = usings.Union(file.Usings).ToList();
             });
 
-            mergedFile = string.Join("\r\n", usings.OrderBy(u => u).Select(u => $"using {u};")) + "\r\n\r\n" + mergedFile;
+            #endregion
+
+            #region Merge
+
+            mergedFileContent = string.Join("\r\n", usings.OrderBy(u => u).Select(u => $"using {u};")) + "\r\n\r\n" + mergedFileContent;
+
+            #endregion
+
+            #region Write Merged File
+
+            File.WriteAllText(outputFileName, mergedFileContent);
+
+            #endregion
+
+            Console.ReadLine();
         }
     }
 }
